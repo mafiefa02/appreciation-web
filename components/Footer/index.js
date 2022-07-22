e5dc9aba244c3180b23a2164f457cc3f3eb3addd
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-22 20:47:19 +0800

    fix font hopefully

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -4,7 +4,7 @@ export default function Footer() {
 
   return (
     <>
-      <div className="absolute bottom-0 w-full flex flex-row justify-center items-center pb-5 pt-5 sm:text-sm text-[10px] bg-neutral-800 text-white">
+      <div className="static w-full flex flex-row justify-center items-center pb-5 pt-5 sm:text-sm text-[10px] bg-neutral-800 text-white">
         <p>
           &copy; {year} Media Komunikasi dan Informasi BPA FMIPA&apos;21 ITB
         </p>
---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* link your fonts here */}
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
