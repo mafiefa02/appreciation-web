17bfd7d538f1e8eea5a518ad59b4c000e525e3f3
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-07-27 20:23:50 +0800

    remove import

--- pages/api/hello.jsx
+++ pages/api/hello.jsx
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { NextApiRequest, NextApiResponse } from "next/dist/shared/lib/utils";
-
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
 }
