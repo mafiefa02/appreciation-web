690f62f6dce07abc04eea0dc773735d57f999343
Afief Abdurrahman <91366510+mafiefa02@users.noreply.github.com>
2022-08-18 06:15:06 +0700

    Add nav props

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,6 +1,9 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Layout from "../../components/Layout";
+import UserProfile from "../../components/UserProfile";
 
 import { prisma } from "../../db.ts";
 
@@ -24,24 +27,47 @@ const Dashboard = ({ posts }) => {
 
   if (post) {
     return (
-      <article className="prose dark:prose-invert prose-h1:uppercase prose-p:text-justify prose-p:leading-loose prose-p:text-background  prose-headings:text-primary-default prose-h1:text-primary-default xl:prose-lg sm:prose-sm prose-base lg:prose-lg prose-p:font-[500]">
-        <h1>Pesan dari MBP</h1>
-        <h4>
-          untuk {user.firstName} {user.lastName}
-        </h4>
-        <p dangerouslySetInnerHTML={{ __html: post.content }} />
-      </article>
+      <Layout loggedIn>
+        <Head>
+          <title>
+            {user.firstName} {user.lastName} | BPA FMIPA&apos;21 ITB
+          </title>
+          <meta
+            name="description"
+            content="Presented by Media Komunikasi dan Informasi BPA FMIPA'21 ITB"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <article className="prose dark:prose-invert prose-h1:uppercase prose-p:text-justify prose-p:leading-loose prose-p:text-background  prose-headings:text-primary-default prose-h1:text-primary-default xl:prose-lg sm:prose-sm prose-base lg:prose-lg prose-p:font-[500]">
+          <UserProfile className={`w-[200px]`} user={user} />
+          <p dangerouslySetInnerHTML={{ __html: post.content }} />
+        </article>
+      </Layout>
     );
   }
 
   return (
     <>
-      <article className="prose text-center dark:prose-invert prose-headings:uppercase prose-p:text-justify prose-p:leading-loose prose-p:text-background prose-headings:text-background xl:prose-lg sm:prose-sm prose-base lg:prose-lg">
-        <h1>Tidak ada pesan untuk</h1>
-        <h3>
-          {user.firstName} {user.lastName}
-        </h3>
-      </article>
+      <Layout loggedIn>
+        <Head>
+          <title>
+            {user.firstName} {user.lastName} | BPA FMIPA&apos;21 ITB
+          </title>
+          <meta
+            name="description"
+            content="Presented by Media Komunikasi dan Informasi BPA FMIPA'21 ITB"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <article className="prose text-center dark:prose-invert prose-headings:uppercase prose-p:text-justify prose-p:leading-loose prose-p:text-background prose-headings:text-background xl:prose-lg sm:prose-sm prose-base lg:prose-lg">
+          <h1>Tidak ada pesan untuk</h1>
+          <UserProfile
+            className={`w-[200px]`}
+            user={user}
+            position={"center"}
+          />
+        </article>
+      </Layout>
     </>
   );
 };
